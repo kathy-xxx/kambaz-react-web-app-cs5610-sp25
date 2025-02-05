58e192d113fd4ec38dcb380ed13373e34f6cc7c0
kathyxiao <xiao.zixua@northeastern.edu>
2025-02-04 21:15:05 -0500

    v 2.4.4

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,27 +1,209 @@
+import { Button, Card, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 export default function Dashboard() {
   return (
-    <div id="wd-dashboard">
-      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />{" "}
-      <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+    <div
+      id="wd-dashboard"
+      className="ms-5 ps-5 mt-3"
+      style={{ marginLeft: "120px" }}
+    >
+      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h2 id="wd-dashboard-published">Published Courses (8)</h2> <hr />
       <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1234/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS1234 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer{" "}
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course"> ... </div>
-        <div className="wd-dashboard-course"> ... </div>
+        <Row xs={1} sm={2} md={4} className="g-4">
+          <Col className="wd-dashboard-course" style={{ width: "250px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src="/images/reactjs.jpg"
+                  width="100%"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS1234 React JS
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Full Stack software developer
+                  </Card.Text>
+                  <Button variant="primary">Go</Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "250px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src="/images/reactjs.jpg"
+                  width="100%"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS1234 React JS
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Full Stack software developer
+                  </Card.Text>
+                  <Button variant="primary">Go</Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "250px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src="/images/reactjs.jpg"
+                  width="100%"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS1234 React JS
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Full Stack software developer
+                  </Card.Text>
+                  <Button variant="primary">Go</Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "250px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src="/images/reactjs.jpg"
+                  width="100%"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS1234 React JS
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Full Stack software developer
+                  </Card.Text>
+                  <Button variant="primary">Go</Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "250px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src="/images/reactjs.jpg"
+                  width="100%"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS1234 React JS
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Full Stack software developer
+                  </Card.Text>
+                  <Button variant="primary">Go</Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "250px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src="/images/reactjs.jpg"
+                  width="100%"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS1234 React JS
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Full Stack software developer
+                  </Card.Text>
+                  <Button variant="primary">Go</Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "250px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src="/images/reactjs.jpg"
+                  width="100%"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS1234 React JS
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Full Stack software developer
+                  </Card.Text>
+                  <Button variant="primary">Go</Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "250px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src="/images/reactjs.jpg"
+                  width="100%"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS1234 React JS
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Full Stack software developer
+                  </Card.Text>
+                  <Button variant="primary">Go</Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+        </Row>
       </div>
     </div>
   );
--- src/Kambaz/Navigation.tsx
+++ src/Kambaz/Navigation.tsx
@@ -1,14 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
 import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
 import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
 
 export default function KambazNavigation() {
+  const location = useLocation();
   return (
     <div
       id="wd-kambaz-navigation"
-      style={{ width: 120 }}
+      style={{ width: 110 }}
       className="list-group rounded-0 position-fixed
        bottom-0 top-0 d-none d-md-block bg-black z-2"
     >
@@ -23,17 +24,30 @@ export default function KambazNavigation() {
       <Link
         to="/Kambaz/Account"
         id="wd-account-link"
-        className="list-group-item text-center border-0 bg-black text-white"
+        className={`list-group-item text-center border-0 py-3 w-100 ${
+          location.pathname === "/Kambaz/Account"
+            ? "bg-white text-danger"
+            : "bg-black text-white"
+        }`}
       >
-        <FaRegCircleUser className="fs-1 text text-white" />
+        <FaRegCircleUser
+          className={`fs-1 ${
+            location.pathname === "/Kambaz/Account"
+              ? "text-danger"
+              : "text-white"
+          }`}
+        />
         <br />
         Account
       </Link>
       <Link
         to="/Kambaz/Dashboard"
         id="wd-dashboard-link"
-        className="list-group-item text-center border-0
-      bg-white text-danger"
+        className={`list-group-item text-center border-0 py-3 w-100 ${
+          location.pathname === "/Kambaz/Dashboard"
+            ? "bg-white text-danger"
+            : "bg-black text-white"
+        }`}
       >
         <AiOutlineDashboard className="fs-1 text-danger" />
         <br />
@@ -42,8 +56,11 @@ export default function KambazNavigation() {
       <Link
         to="/Kambaz/Dashboard"
         id="wd-course-link"
-        className="list-group-item text-white
-      bg-black text-center border-0"
+        className={`list-group-item text-center border-0 py-3 w-100 ${
+          location.pathname === "/Kambaz/Dashboard"
+            ? "bg-white text-danger"
+            : "bg-black text-white"
+        }`}
       >
         <LiaBookSolid className="fs-1 text-danger" />
         <br />
@@ -52,8 +69,11 @@ export default function KambazNavigation() {
       <Link
         to="/Kambaz/Calendar"
         id="wd-calendar-link"
-        className="list-group-item text-white
-      bg-black text-center border-0"
+        className={`list-group-item text-center border-0 py-3 w-100 ${
+          location.pathname === "/Kambaz/Calendar"
+            ? "bg-white text-danger"
+            : "bg-black text-white"
+        }`}
       >
         <IoCalendarOutline className="fs-1 text-danger" />
         <br />
@@ -62,8 +82,11 @@ export default function KambazNavigation() {
       <Link
         to="/Kambaz/Inbox"
         id="wd-inbox-link"
-        className="list-group-item text-white
-                  bg-black text-center border-0"
+        className={`list-group-item text-center border-0 py-3 w-100 ${
+          location.pathname === "/Kambaz/Inbox"
+            ? "bg-white text-danger"
+            : "bg-black text-white"
+        }`}
       >
         <FaInbox className="fs-1 text-danger" />
         <br />
@@ -72,8 +95,11 @@ export default function KambazNavigation() {
       <Link
         to="/Labs"
         id="wd-labs-link"
-        className="list-group-item text-white
-                  bg-black text-center border-0"
+        className={`list-group-item text-center border-0 py-3 w-100 ${
+          location.pathname === "/Labs"
+            ? "bg-white text-danger"
+            : "bg-black text-white"
+        }`}
       >
         <LiaCogSolid className="fs-1 text-danger" />
         <br />
