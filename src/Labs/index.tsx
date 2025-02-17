b1e4d0623b05b74aa89c7919a8129acdd77b39ae
kathyxiao <xiao.zixua@northeastern.edu>
2025-02-17 15:07:16 -0500

    v 3.3.3

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,20 +1,8 @@
-import TOC from "./TOC";
-import Lab1 from "./Lab1";
-import Lab2 from "./Lab2";
-import Lab3 from "./Lab3";
-import { Routes, Route, Navigate } from "react-router-dom";
-export default function Labs() {
+export default function Lab3() {
   return (
-    <div id="wd-labs">
-      <h1>Labs</h1>
-      <TOC />
-      <p>Created by: Zixuan Xiao</p>
-      <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
-        <Route path="Lab1" element={<Lab1 />} />
-        <Route path="Lab2/*" element={<Lab2 />} />
-        <Route path="Lab3" element={<Lab3 />} />
-      </Routes>
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
     </div>
   );
 }
+
