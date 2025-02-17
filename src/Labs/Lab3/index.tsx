1763aadc4cdb3af2c140b3c0f10ad06d373d1d47
kathyxiao <xiao.zixua@northeastern.edu>
2025-02-17 15:17:10 -0500

    v 3.1.6

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,10 +1,22 @@
+import BooleanVariables from "./BooleanVariables";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
 import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
 
 export default function Lab3() {
   return (
     <div id="wd-lab3">
       <h3>Lab 3</h3>
       <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
     </div>
   );
 }
