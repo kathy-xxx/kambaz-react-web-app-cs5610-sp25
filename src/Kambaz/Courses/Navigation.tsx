866df222a78274f86a4bf55d75b5a53873bec969
kathyxiao <xiao.zixua@northeastern.edu>
2025-02-18 17:14:47 -0500

    v 3.7.7

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -2,15 +2,16 @@ import { ListGroup } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
 export default function CourseNavigation() {
   const { pathname } = useLocation();
+  const { cid } = useParams();
   const links = [
-    { label: "Home", path: "/Kambaz/Courses/:cid/Home" },
-    { label: "Modules", path: "/Kambaz/Courses/:cid/Modules" },
+    { label: "Home", path: `/Kambaz/Courses/${cid}/Home` },
+    { label: "Modules", path: `/Kambaz/Courses/${cid}/Modules` },
     { label: "Piazza", path: "https://piazza.com/class/m5h5iqr0wds2l5" },
     { label: "Zoom", path: "https://applications.zoom.us/lti/rich" },
-    { label: "Assignments", path: "/Kambaz/Courses/:cid/Assignments" },
-    { label: "Quizzes", path: "/Kambaz/Courses/:cid/Quizzes" },
-    { label: "Grades", path: "/Kambaz/Courses/:cid/Grades" },
-    { label: "People", path: "/Kambaz/Courses/:cid/People" },
+    { label: "Assignments", path: `/Kambaz/Courses/${cid}/Assignments` },
+    { label: "Quizzes", path: `/Kambaz/Courses/${cid}/Quizzes` },
+    { label: "Grades", path: `/Kambaz/Courses/${cid}/Grades` },
+    { label: "People", path: `/Kambaz/Courses/${cid}/People` },
   ];
   return (
     <ListGroup
