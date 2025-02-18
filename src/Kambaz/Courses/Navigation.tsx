674bcc38ca749b64f1a00c8ac1ca6f0203b7512c
kathyxiao <xiao.zixua@northeastern.edu>
2025-02-18 16:01:18 -0500

    v 3.7.5

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -1,65 +1,33 @@
-import { Link } from "react-router-dom";
+import { ListGroup } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
 export default function CourseNavigation() {
-
+  const { pathname } = useLocation();
+  const links = [
+    { label: "Home", path: "/Kambaz/Courses/:cid/Home" },
+    { label: "Modules", path: "/Kambaz/Courses/:cid/Modules" },
+    { label: "Piazza", path: "https://piazza.com/class/m5h5iqr0wds2l5" },
+    { label: "Zoom", path: "https://applications.zoom.us/lti/rich" },
+    { label: "Assignments", path: "/Kambas/Courses/:cid/assignments" },
+    { label: "Quizzes", path: "/Kambas/Courses/:cid/quizzes" },
+    { label: "Grades", path: "/Kambaz/Courses/:cid/Grades" },
+    { label: "People", path: "/Kambaz/Courses/:cid/People" },
+  ];
   return (
-    <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <Link
-        to="/Kambaz/Courses/1234/Home"
-        id="wd-course-home-link"
-        className="list-group-item active border border-0"
-      >
-        Home
-      </Link>
-      <Link
-        to="/Kambaz/Courses/1234/Modules"
-        id="wd-course-modules-link"
-        className="list-group-item text-danger border border-0"
-      >
-        Modules
-      </Link>
-      <Link
-        to="https://piazza.com/class/m5h5iqr0wds2l5"
-        id="wd-course-piazza-link"
-        className="list-group-item text-danger border border-0"
-      >
-        Piazza
-      </Link>
-      <Link
-        to="https://applications.zoom.us/lti/rich"
-        id="wd-course-zoom-link"
-        className="list-group-item text-danger border border-0"
-      >
-        Zoom
-      </Link>
-      <Link
-        to="/Kambaz/Courses/1234/Assignments"
-        id="wd-course-quizzes-link"
-        className="list-group-item text-danger border border-0"
-      >
-        Assignments
-      </Link>
-      <Link
-        to="/Kambaz/Courses/1234/Quizzes"
-        id="wd-course-assignments-link"
-        className="list-group-item text-danger border border-0"
-      >
-        Quizzes
-      </Link>
-      <Link
-        to="/Kambaz/Courses/1234/Grades"
-        id="wd-course-grades-link"
-        className="list-group-item text-danger border border-0
-      "
-      >
-        Grades
-      </Link>
-      <Link
-        to="/Kambaz/Courses/:cid/People"
-        id="wd-course-people-link"
-        className="list-group-item text-danger border border-0"
-      >
-        People
-      </Link>
-    </div>
+    <ListGroup
+      id="wd-courses-navigation"
+      className="wd list-group fs-5 rounded-0"
+    >
+      {links.map((link) => (
+        <ListGroup.Item
+          key={link.path}
+          as={Link}
+          to={link.path}
+          className={`list-group-item text-danger border border-0
+         ${pathname.includes(link.label) ? "active" : "text-danger"}`}
+        >
+          {link.label}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
   );
 }
