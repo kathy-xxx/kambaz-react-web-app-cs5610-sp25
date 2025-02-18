f3eba4c47619490cce9714170f5074705e0c44a4
kathyxiao <xiao.zixua@northeastern.edu>
2025-02-18 16:12:19 -0500

    v 3.7.5

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -7,8 +7,8 @@ export default function CourseNavigation() {
     { label: "Modules", path: "/Kambaz/Courses/:cid/Modules" },
     { label: "Piazza", path: "https://piazza.com/class/m5h5iqr0wds2l5" },
     { label: "Zoom", path: "https://applications.zoom.us/lti/rich" },
-    { label: "Assignments", path: "/Kambas/Courses/:cid/assignments" },
-    { label: "Quizzes", path: "/Kambas/Courses/:cid/quizzes" },
+    { label: "Assignments", path: "/Kambas/Courses/:cid/Assignments" },
+    { label: "Quizzes", path: "/Kambas/Courses/:cid/Quizzes" },
     { label: "Grades", path: "/Kambaz/Courses/:cid/Grades" },
     { label: "People", path: "/Kambaz/Courses/:cid/People" },
   ];
