02a1eb75f9fd770ad7984c0e1602e464b51be63c
kathyxiao <xiao.zixua@northeastern.edu>
2025-03-12 14:10:07 -0400

    v 4.1

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,6 +2,7 @@ import TOC from "./TOC";
 import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import Lab4 from "./Lab4";
 import { Routes, Route, Navigate } from "react-router-dom";
 export default function Labs() {
   return (
@@ -14,6 +15,7 @@ export default function Labs() {
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2/*" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
+        <Route path="Lab4" element={<Lab4 />} />
       </Routes>
     </div>
   );
