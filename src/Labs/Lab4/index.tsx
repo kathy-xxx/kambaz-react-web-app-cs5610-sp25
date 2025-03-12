307bf84a84d2a7bb98945959b267988ea6abba4b
kathyxiao <xiao.zixua@northeastern.edu>
2025-03-12 14:18:42 -0400

    v 4.2.2.2

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,4 +1,5 @@
 import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
 import EventObject from "./EventObject";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
@@ -14,6 +15,7 @@ export default function Lab4() {
       <PassingDataOnEvent />
       <PassingFunctions theFunction={sayHello} />
       <EventObject />
+      <Counter />
     </div>
   );
 }
