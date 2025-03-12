6313d4389b67554232ad47b38d8aa1aa2a8add69
kathyxiao <xiao.zixua@northeastern.edu>
2025-03-12 15:03:41 -0400

    v 4.3.5.2

--- src/Labs/Lab4/ReduxExamples/HelloRedux/index.tsx
+++ src/Labs/Lab4/ReduxExamples/HelloRedux/index.tsx
@@ -1,16 +1,10 @@
-import { useSelector } from "react-redux";
-import CounterRedux from "./CounterRedux";
-import AddRedux from "./AddRedux";
-import TodoList from "./todos/TodoList";
+import { useSelector, useDispatch } from "react-redux";
 export default function HelloRedux() {
   const { message } = useSelector((state: any) => state.helloReducer);
   return (
     <div id="wd-hello-redux">
       <h3>Hello Redux</h3>
       <h4>{message}</h4> <hr />
-      <CounterRedux />
-      <AddRedux />
-      <TodoList />
     </div>
   );
 }
