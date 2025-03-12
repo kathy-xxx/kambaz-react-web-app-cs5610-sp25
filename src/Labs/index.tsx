d8520c0ff421b02692c62bfa1c710594adb4028d
kathyxiao <xiao.zixua@northeastern.edu>
2025-03-12 14:39:06 -0400

    v 4.3.3

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,20 +3,24 @@ import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 import Lab4 from "./Lab4";
+import store from "./store";
+import { Provider } from "react-redux";
 import { Routes, Route, Navigate } from "react-router-dom";
 export default function Labs() {
   return (
-    <div id="wd-labs">
-      <h1>Labs</h1>
-      <TOC />
-      <p>Created by: Zixuan Xiao</p>
-      <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
-        <Route path="Lab1" element={<Lab1 />} />
-        <Route path="Lab2/*" element={<Lab2 />} />
-        <Route path="Lab3" element={<Lab3 />} />
-        <Route path="Lab4" element={<Lab4 />} />
-      </Routes>
-    </div>
+    <Provider store={store}>
+      <div className="container-fluid">
+        <h1>Labs</h1>
+        <TOC />
+        <p>Created by: Zixuan Xiao</p>
+        <Routes>
+          <Route path="/" element={<Navigate to="Lab1" />} />
+          <Route path="Lab1" element={<Lab1 />} />
+          <Route path="Lab2/*" element={<Lab2 />} />
+          <Route path="Lab3" element={<Lab3 />} />
+          <Route path="Lab4" element={<Lab4 />} />
+        </Routes>
+      </div>
+    </Provider>
   );
 }
