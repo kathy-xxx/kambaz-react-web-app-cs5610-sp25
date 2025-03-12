4d1f3700568ced7be3e4fabeabaa9194bbca4ddc
kathyxiao <xiao.zixua@northeastern.edu>
2025-03-12 14:15:57 -0400

    v 4.2.1

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,7 +1,19 @@
+import ClickEvent from "./ClickEvent";
+import EventObject from "./EventObject";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+
 export default function Lab4() {
+  function sayHello() {
+    alert("Hello");
+  }
   return (
-    <div id="wd-lab3">
+    <div id="wd-lab4">
       <h3>Lab 4</h3>
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
     </div>
   );
 }
