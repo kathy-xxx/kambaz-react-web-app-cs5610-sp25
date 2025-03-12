111a92a7228c7ee8f4e9b3d0257fc9f1bc552cc1
kathyxiao <xiao.zixua@northeastern.edu>
2025-03-12 16:08:14 -0400

    v 4.4.3.4

--- src/Kambaz/Courses/Modules/ModulesControls.tsx
+++ src/Kambaz/Courses/Modules/ModulesControls.tsx
@@ -2,11 +2,26 @@ import { FaPlus } from "react-icons/fa6";
 import GreenCheckmark from "./GreenCheckmark";
 import { Button, Dropdown } from "react-bootstrap";
 import { IoBan } from "react-icons/io5";
-export default function ModulesControls() {
+import ModuleEditor from "./ModuleEditor";
+import { useState } from "react";
+export default function ModulesControls({
+  moduleName,
+  setModuleName,
+  addModule,
+}: {
+  moduleName: string;
+  setModuleName: (title: string) => void;
+  addModule: () => void;
+}) {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div id="wd-modules-controls" className="text-nowrap">
       <Button
         variant="danger"
+        onClick={handleShow}
         size="lg"
         className="me-1 float-end"
         id="wd-add-module-btn"
@@ -52,6 +67,14 @@ export default function ModulesControls() {
       >
         Collapse All
       </Button>
+      <ModuleEditor
+        show={show}
+        handleClose={handleClose}
+        dialogTitle="Add Module"
+        moduleName={moduleName}
+        setModuleName={setModuleName}
+        addModule={addModule}
+      />
     </div>
   );
 }
--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -40,7 +40,7 @@ export default function Kambaz() {
       <KambazNavigation />
       <div className="wd-main-content-offset p-3">
         <Routes>
-          <Route path="/" element={<Navigate to="Account" />} />
+          <Route path="/" element={<Navigate to="Dashboard" />} />
           <Route path="/Account/*" element={<Account />} />
           <Route
             path="/Dashboard"
@@ -55,7 +55,10 @@ export default function Kambaz() {
               />
             }
           />
-          <Route path="/Courses/:cid/*" element={<Courses />} />
+          <Route
+            path="/Courses/:cid/*"
+            element={<Courses courses={courses} />}
+          />
           <Route path="/Calendar" element={<h1>Calendar</h1>} />
           <Route path="/Inbox" element={<h1>Inbox</h1>} />
         </Routes>
