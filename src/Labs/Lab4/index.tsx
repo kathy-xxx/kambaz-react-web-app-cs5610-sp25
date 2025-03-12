1418a5e11eb63ff4349a371eb99f56b49d69c106
kathyxiao <xiao.zixua@northeastern.edu>
2025-03-12 14:24:13 -0400

    v 4.2.2.6

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,8 +1,12 @@
+import BooleanStateVariables from "./BooleanStateVariables";
 import ClickEvent from "./ClickEvent";
 import Counter from "./Counter";
+import DateStateVariable from "./DateStateVariable";
 import EventObject from "./EventObject";
+import ObjectStateVariable from "./ObjectStateVariable";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import StringStateVariables from "./StringStateVariables";
 
 export default function Lab4() {
   function sayHello() {
@@ -16,6 +20,10 @@ export default function Lab4() {
       <PassingFunctions theFunction={sayHello} />
       <EventObject />
       <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable />
+      <ObjectStateVariable />
     </div>
   );
 }
