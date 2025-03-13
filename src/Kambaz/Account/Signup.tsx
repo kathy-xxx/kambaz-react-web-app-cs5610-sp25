c0c52ded60db0a3363cd5d43f4fd7a6df18ad238
kathyxiao <xiao.zixua@northeastern.edu>
2025-03-12 21:49:38 -0400

    a4 Redux

--- src/Kambaz/Account/Signup.tsx
+++ src/Kambaz/Account/Signup.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Signup() {
   return (
     <div id="wd-signin-screen">
-      <h3>Sign up</h3>
+      <h1>Sign up</h1>
       <Form.Control id="wd-username" placeholder="username" className="mb-2" />
       <Form.Control
         id="wd-password"
