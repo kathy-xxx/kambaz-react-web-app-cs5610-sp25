2fd8127b170f0119d1826b105a4901d4afb5a277
kathyxiao <xiao.zixua@northeastern.edu>
2025-03-12 21:47:57 -0400

    a4 Redux

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,15 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
+
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const location = useLocation();
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
       {links.map((link) => (
         <Link
-          to="`/Kambaz/Account/${link}`"
-          id="wd-account-signin-link"
-          className="list-group-item active border border-0"
+          to={`/Kambaz/Account/${link}`}
+          id={`wd-course-${link.toLowerCase()}-link`}
+          className={`list-group-item border border-0 ${
+            location.pathname === `/Kambaz/Account/${link}`
+              ? "active"
+              : "text-danger"
+          }`}
         >
           {link}
         </Link>
